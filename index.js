2d54186544b6681622ce9e5b5b8cfc6788878ce6
Adeleke Hammed <lekhad19@gmail.com>
2024-02-24 21:57:16 +0100

    prisma added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express'); 
 const app = express(); 
-const {} = 
+const { PrismaClient } = require("@prisma/client");
 app.use(express.json()); 
-app.get("/", (req, res) => {
 
+const prisma = new PrismaClient(); 
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    const allUsers = await prisma.users.findMany();
+    res.json(allUsers);
+});
+
+app.post("/", async (req, res) => {
+    
+    const allUsers = await prisma.users.findMany();
+    res.json(allUsers);
 });
 
 app.listen(3001, () => console.log(`Server running on port ${3001}`)); 
