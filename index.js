ae75f18a59b6bdbb7ed3865be0ca5a9b6df64983
Adeleke Hammed <lekhad19@gmail.com>
2024-02-24 21:50:39 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express'); 
 const app = express(); 
-
+const {} = 
 app.use(express.json()); 
+app.get("/", (req, res) => {
+
+});
+
 app.listen(3001, () => console.log(`Server running on port ${3001}`)); 
