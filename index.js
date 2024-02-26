05e188f64215e36646b3b53efdb3ab8e54357ee1
Adeleke Hammed <lekhad19@gmail.com>
2024-02-26 08:49:38 +0100

    create, read, update with prisma

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ app.get("/", async (req, res) => {
     res.json(allUsers);
 });
 
-app.post("/", async (req, res) => {
-    
-    const allUsers = await prisma.users.findMany();
-    res.json(allUsers);
+app.post("/", async (req, res) => { 
+    const newUser = await prisma.users.create({ data : req.body });
+    res.json(newUser);
+});
+
+
+app.put("/:id", async (req, res) => { 
+    const id = req.params.id; 
+    const newAge = req.body.age;
+    const updatedUser = await prisma.users.update({ 
+        where: { id: parseInt(id) }, 
+        data : { age : newAge }, 
+     });
+    res.json(updatedUser);
 });
 
 app.listen(3001, () => console.log(`Server running on port ${3001}`)); 
